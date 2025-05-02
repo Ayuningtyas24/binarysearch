27af77f39a938cf3b70ce59323ee8ccdb71c902e
Ayuningtyas24 <adyahseptiani@gmail.com>
2025-05-02 09:18:23 +0700

    menambahkan void display dan memanggil method

--- file1.cpp
+++ file1.cpp
@@ -51,6 +51,23 @@ void bubbleSortArray()
     } while (pass <= nPanjang - 1); // step 5
 }
 
+void display()
+{
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " --> ";
+        }
+    }
+}
+
 void binarySearch()
 {
     cout << "Masukkan element yang ingin dicari =";
@@ -86,5 +103,9 @@ void binarySearch()
 
 int main()
 {
+    input();
+    bubbleSortArray();
+    display();
+    binarySearch();
 
 }
